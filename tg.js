bc5a9684efc02d4bfe7629dfb71f7f4f998fd7e6
Alexey Gaev <gaevalexey@gmail.com>
2010-11-18 13:23:31 +0200

    Figures are falling. Pretty dummy.

--- tg.js
+++ tg.js
@@ -42,6 +42,7 @@ Tetris.prototype.draw = function() {
 	$(gameInfo).append('<p class=tg_info_p id=tg_level_text>1</p>');
 	var gameInfoNext = $('.tg_info_next', gameInfo);
 	this.preview = $(gameInfoNext);
+	this.field = $(gameField);
 	$(gameInfoNext).css('width', this.PREVIEW_BLOCK_SIZE*(this.FIGURE_MAX_SIZE+2)+'px').css('height', this.PREVIEW_BLOCK_SIZE*(this.FIGURE_MAX_SIZE+2)+'px');
 	for (i=0; i<this.FIGURE_MAX_SIZE+2; i++)
 		for (j=0; j<this.FIGURE_MAX_SIZE+2; j++)
@@ -74,20 +75,45 @@ Tetris.prototype.start = function() {
 	var scoreForLevel = 20; //Score needed for level
 	var scoreForLevelNext = 1.5; //Score append for every next level
 	var level = 1; //Starting level
-	var levelSpeed = 2000; //Speed for level 1 (timeout in ms)
+	var levelSpeed = 500; //Speed for level 1 (timeout in ms)
 	var levelSpeedNext = 0.8; //Speed boost for every level;
 
+	var gameOver = false; //game over indicator
+
+	var currentPreviewFigure; //figure, that is displaying in next field
+
 	makePreview(this.figures, this.preview, this.PREVIEW_BLOCK_SIZE);
+
+	$(this.field).append('<div class=tg_figure_fall></div>');
+	var figureFallDiv = $('.tg_figure_fall', this.field);
+
+	var figureFall = this.figures[Math.floor(Math.random()*this.figures.length)];
+
+	figureFall.draw(figureFallDiv);
+	$('.tg_figure_block', figureFallDiv).css('width', this.blockSize + 'px').css('height', this.blockSize + 'px');
+	$('.tg_figure_block.colored', figureFallDiv).css('backgroundColor', figureFall.color);
+	$(figureFallDiv).css('left', Math.floor((this.width-figureFall.presentation.length+1)/2)*this.blockSize+'px').css('top','0').css('width', this.blockSize*figureFall.presentation.length+'px');
+	
+	this.timeout = setTimeout(fall, levelSpeed, this.blockSize, false);
+
+	function fall(offset, stop) {
+		$(figureFallDiv).css('top', (parseInt($(figureFallDiv).css('top'))+offset)+'px');
+		if (!stop) t=setTimeout(moveFigure, levelSpeed, offset);
+	}
+
+	function checkCollision(direction) {
+		return true;
+	}
 
 	function makePreview(figures, previewObj, previeBlockSize) {
 		if (!$('.tg_figure_preview', previewObj).length) $(previewObj).append('<div class=tg_figure_preview></div>');
 		var previewDiv = $('.tg_figure_preview', previewObj);
-		var figure = figures[Math.floor(Math.random()*figures.length)];
-		var previewFSize = figure.presentation.length;
+		var currentPreviewFigure = figures[Math.floor(Math.random()*figures.length)];
+		var previewFSize = currentPreviewFigure.presentation.length;
 		$(previewDiv).css('width', previewFSize*previeBlockSize + 'px').css('height', previewFSize*previeBlockSize + 'px').css('left', previeBlockSize+'px').css('top',previeBlockSize+'px');
-		figure.draw(previewDiv);
+		currentPreviewFigure.draw(previewDiv);
 		$('.tg_figure_block', previewDiv).css('width', previeBlockSize + 'px').css('height', previeBlockSize + 'px');
-		$('.tg_figure_block.colored', previewDiv).css('backgroundColor', figure.color);
+		$('.tg_figure_block.colored', previewDiv).css('backgroundColor', currentPreviewFigure.color);
 	}
 }
 Tetris.prototype.pause = function() {}
@@ -114,5 +140,6 @@ Figure.prototype.draw = function(container) {
 			$(container).append('<div class="tg_figure_block '+blockClass+'"></div>');
 		}
 }
+
 
 Figure.prototype.rotate = function() {}
